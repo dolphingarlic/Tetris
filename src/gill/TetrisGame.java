a58121ddd563d99d1696ddbfb4c122c3d067bd1a
Andi Qu <19573@stjohnscollege.co.za>
2019-10-08 15:12:51 +0200

    Added block sprites

--- src/gill/TetrisGame.java
+++ src/gill/TetrisGame.java
@@ -18,7 +18,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class TetrisGame extends ApplicationAdapter {
     
     Sprite background;
+    Sprite[] blocks;
+
     SpriteBatch batch;
+    
+    int BOARD_HEIGHT = 20, BOARD_WIDTH = 10;
+    int[][] board = new int[BOARD_HEIGHT][BOARD_WIDTH];
     
     @Override
     public void create() {
@@ -26,6 +31,12 @@ public class TetrisGame extends ApplicationAdapter {
         background = new Sprite(tex);
         background.setPosition(690, 45);
         batch = new SpriteBatch();
+        
+        blocks = new Sprite[8];
+        for (int i = 1; i < 8; i++) {
+            tex = new Texture("images/block" + i + ".png");
+            blocks[i] = new Sprite(tex);
+        }
     }
     
     @Override
@@ -33,8 +44,18 @@ public class TetrisGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0.2f, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         
+        
         batch.begin();
         background.draw(batch);
+        for (int i = 0; i < BOARD_HEIGHT; i++) {
+            for (int j = 0; j < BOARD_WIDTH; j++) {
+                board[i][j] = (int)Math.floor(Math.random() * 7) + 1;
+                
+                Sprite block = blocks[board[i][j]];
+                block.setPosition(735 + 45 * j, 90 + 45 * i);
+                block.draw(batch);
+            }
+        }
         batch.end();
     }
     
